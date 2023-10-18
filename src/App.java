c81ef62185c9c2938505d94122b7b0518174a4da
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2023-10-18 10:38:01 +0200

    Gjort klar del a

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //a
+        int litetTal = 10;
+        int storTal = 1000;
+        if(litetTal<1000)
+        {
+            System.out.println(litetTal+" mindre än "+storTal);
+        }
+        else
+        {
+            System.out.println(litetTal+" mindre än "+storTal);
+        }
+        System.out.println("litetTal = "+litetTal+", storTal = "+storTal);
+
+        //Koden fungerar inte eftersom variabeln "storTal inte är definerad i else och sout."
+        //Koden fungerar genom att flytta "int storTal = 1000;" ut från if då det endast gällde för den.
+
+        
     }
 }
